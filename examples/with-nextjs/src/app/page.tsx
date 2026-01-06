368e26d7a77a75065be6e72eef866ededcd7d8fa
yguo <guoyi1023@gmail.com>
2026-01-06 12:07:54 +0800

    uncomment the app router.

--- examples/with-nextjs/src/app/page.tsx
+++ examples/with-nextjs/src/app/page.tsx
@@ -15,11 +15,11 @@ const ExcalidrawWithClientOnly = dynamic(
 export default function Page() {
   return (
     <>
-      <a href="/excalidraw-in-pages">Switch to Pages router</a>
+      {/* <a href="/excalidraw-in-pages">Switch to Pages router</a>
       <h1 className="page-title">App Router</h1>
       <Script id="load-env-variables" strategy="beforeInteractive">
         {`window["EXCALIDRAW_ASSET_PATH"] = window.origin;`}
-      </Script>
+      </Script> */}
       {/* @ts-expect-error - https://github.com/vercel/next.js/issues/42292 */}
       <ExcalidrawWithClientOnly />
     </>
