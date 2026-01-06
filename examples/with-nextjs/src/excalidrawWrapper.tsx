d1233995cfc7835335142a5c771c24133dd77d91
yguo <guoyi1023@gmail.com>
2026-01-06 14:15:12 +0800

    comment

--- examples/with-nextjs/src/excalidrawWrapper.tsx
+++ examples/with-nextjs/src/excalidrawWrapper.tsx
@@ -9,13 +9,13 @@ import App from "../../with-script-in-browser/components/ExampleApp";
 const ExcalidrawWrapper: React.FC = () => {
   return (
     <>
-      {/* <App
-        appTitle={"Excalidraw with Nextjs Example"}
-        useCustom={(api: any, args?: any[]) => {}}
+      <App
+        appTitle={"Eric Draw"}
+        useCustom={(api: any, args?: any[]) => { }}
         excalidrawLib={excalidrawLib}
-      > */}
-      <Excalidraw />
-      {/* </App> */}
+      >
+        <Excalidraw />
+      </App>
     </>
   );
 };
